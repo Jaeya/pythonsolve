052c90b8a1b343d07c5bda53e260e3896b69e0fd
Jaeya <87467649+Jaeya@users.noreply.github.com>
2021-07-19 09:27:44 +0900

    Add files via upload

---
+++ Solve.py
@@ -0,0 +1,29 @@
+print(2+3)
+#이렇게 주석을 사용 할 수 있습니다. ctrl + / 전체 주석
+
+print("Hello World")
+
+x =2
+y =3
+print (x+y)
+
+print("오늘은" , "금요일")
+
+print("BITCAMP" , "UNITY" , "SANGJAE" ,sep = "/")
+
+print("First" , end = "")
+print("Second")
+
+print(5/3)
+
+삼성전자 = 80000
+총평가금액 = 삼성전자 * 10
+print("총평가금액 = " ,총평가금액)
+
+시가총액 = 290000000000
+현재가 = 5000
+PER = 15.79
+print(시가총액, type(시가총액))
+print(현재가 , type(현재가))
+print(PER , type(PER))
+
